9c2a56cb88fdc2a0622abcb65412dec81da2c673
Tiago <tiagowebersc@gmail.com>
2019-07-11 13:23:20 +0200

    script for the database

---
+++ sql/sqlCommands.sql
@@ -0,0 +1,21 @@
+create database real_state;
+
+create table typeHousing (
+  id_type int(10) primary key not null auto_increment,
+  description varchar(50) not null
+)ENGINE=INNODB;
+
+create table housing (
+ id_housing int(10) primary key not null auto_increment,
+ title varchar(50) not null,
+ address varchar(100) not null,
+ city varchar(50) not null,
+ pc varchar(4) not null,
+ area float(6,2) not null,
+ price float(10,2) not null,
+ photo varchar(200) not null,
+ id_type int(10) not null,
+ description varchar(500) not null,
+ CONSTRAINT fk_id_type Foreign Key(id_type) REFERENCES typeHousing(id_type)
+)ENGINE=INNODB;
+
